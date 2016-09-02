90824d720b71feda27f34fcea50e01b4feab8de5
Nizar  El Ghallab <niiizax_officiel@hotmail.fr>
2016-09-02 17:59:37 +0100

    Add project files.

---
+++ DataTypes_CharStringBoolTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes_CharStringBoolTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int charMin = char.MinValue;
+            int charMax = char.MaxValue;
+
+            char MyVar = 'G';
+            int MyVarValue = 'G';
+            char MyVarValue2 = Convert.ToChar(75);
+            Console.WriteLine("Min Char = {0} Max char = {1} My Character = {2} My Variable Number = {3} my conversion value is {4}"
+                , charMin, charMax, MyVar, MyVarValue, MyVarValue2);
+
+            string myString = "My Value is a Variable";
+            Console.WriteLine("\nMy String is {0}", myString);
+
+            bool mybool = true;
+            Console.WriteLine("My Boolean Value is {0}", mybool);
+           
+            Console.ReadKey();
+        }
+    }
+}
